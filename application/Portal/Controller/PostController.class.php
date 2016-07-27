09dbf1efdc88ede925a6331783a8597018820b66
szlfwolf <szlfwolf@outlook.com>
2016-07-28 01:03:39 +0800

    update

--- application/Portal/Controller/PostController.class.php
+++ application/Portal/Controller/PostController.class.php
@@ -13,19 +13,52 @@ use Common\Controller\HomebaseController;
 */
 class PostController extends HomebaseController {
 
+	protected $posts_model;
+	protected $term_relationships_model;
+	protected $terms_model;
+	
+	function _initialize() {
+		parent::_initialize();
+		$this->posts_model = D("Portal/Posts");
+		$this->terms_model = D("Portal/Terms");
+		$this->term_relationships_model = D("Portal/TermRelationships");
+	}
+	
 	//文章内页
 	public function index() {
 		
-		$this->theme(C('DEFAULT_THEME'))->display("post");
+		$this->theme(C('DEFAULT_THEME'))->display(":post");
 	}
 	
 	public function article(){
-		$this->display("article");
+		if(sp_is_user_login())
+		{
+			$this->display("article");
+		}else{
+			
+			$_SESSION['login_http_referer'] = U('Post/article');
+			//$this->error("请先登录！", $redirect, 1);
+			$this->redirect(U('user/login/index'));
+		}
+		
 	}
 	
 	public function post_article(){
 		if(IS_POST){
-			$this->display(":post");
+			$posts_model = D("Portal/Posts");
+			$_POST['post']['post_date']=date("Y-m-d H:i:s",time());
+			$_POST['post']['post_author']=sp_get_current_userid();
+			
+			dump(I("post.title"));
+			$article=I("post.post");
+			
+			$article['post_title']='a title';//htmlspecialchars_decode($_POST['title']);
+			$article['post_content']='content'; //htmlspecialchars_decode($_POST['content']);
+			$result=$this->posts_model->add($article);
+			if ($result) {
+				$this->redirect(U('article/index'),array("id"=>$result["id"]));
+				
+			}
 		}
 	}
 	
